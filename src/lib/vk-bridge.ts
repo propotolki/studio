13dfff310594b1fd4598cd5f89c52064728786d5
propotolki <propotolki52@gmail.com>
2026-05-01 14:31:23 +0300

    Delete src directory

--- src/lib/vk-bridge.ts
+++
@@ -1,9 +0,0 @@
-import bridge from '@vkontakte/vk-bridge';
-
-export async function initVkMiniApp() {
-  await bridge.send('VKWebAppInit');
-}
-
-export async function getVkUserInfo() {
-  return bridge.send('VKWebAppGetUserInfo');
-}
